84e8c9a1e62e69cc980bf080ffff5d96fd86b000
YaredMekonnen <yaredmekonnen636@gmail.com>
2023-02-12 14:42:42 -0600

    Routes

--- src/App.js
+++ src/App.js
@@ -1,88 +1,23 @@
 import './index.css';
-import Employee from './components/employee'
-import {useState} from 'react'
-import {v4 as uuidv4} from 'uuid'
-import Addemployee from './components/addemployee';
-
+import Header from './components/Header';
+import Employees from './pages/Employees';
+import Customers from './pages/Customers';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 function App() {
-console.log("You Know What we doing")
-const showEmployee = true
-const [employees,setemployess] = useState([
-  {
-    id:1,
-    name:'Yared',
-    role:'CEO',
-    img:'https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg'
-  },
-  {id:2,
-    name:'Yare',
-    role:'CEO',
-    img:'https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg'
-  },
-  {id:3,
-    name:'Yar',
-    role:'CEO',
-    img:'https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg'
-  },
-  {id:4,
-    name:'Ya',
-    role:'CEO',
-    img:'https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg'
-  },
-  {id:5,
-    name:'Y',
-    role:'CEO',
-    img:'https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg'
-  },{id:6,
-    name:'Yada',
-    role:'CEO',
-    img:'https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg'
-  }
-])
+    return (
+        <BrowserRouter>
+        <Header>
+        <Routes>
+        
+            <Route path='/employees' element={<Employees />} />
+            <Route path='/customers' element={<Customers />} />
+       
+        </Routes>
+        </Header>
 
-function updateEmployee(id,newName,newRole){
-   const updated = employees.map(employee=>{
-    if(id === employee.id){
-      return{...employee,name:newName,role:newRole}
-    }
-    return employee;
-   })
-   setemployess(updated);
-}
-
-
-function addEmployee(name,role,img){
- const newemployee = {
-  id: uuidv4(),
-  name: name,
-  role: role,
-  img: img
- }
-
-setemployess([...employees,newemployee])
-
-}
-  return (
-    <div className="App">
-     {showEmployee ? (
-     <>
-     <div className='flex flex-wrap justify-center'>
-        {employees.map((employee)=>{
-          return(<Employee key={employee.id}
-                           id={employee.id}
-                           name={employee.name} 
-                           role={employee.role} 
-                           img={employee.img}
-                           updateEmployee={updateEmployee}/>)
-
-        })}
-        {console.log(uuidv4())}
-      </div>
-      <Addemployee addEmployee={addEmployee}/>
-      </>
-     ):"You ain't seeing no employee"}
-    </div>
-  );
+        </BrowserRouter>
+        
+    );
 }
 
 export default App;
---
+++ src/pages/Customers.js
@@ -0,0 +1,7 @@
+function Customers(){
+    return (
+        <h1> Hello World</h1>
+    )
+} 
+
+export default Customers;
